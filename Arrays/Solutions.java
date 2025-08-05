ec08a0e3272d1ee87a351c923cf6b133409c1b50
MerlineAngel <merlineangel6@gmail.com>
2025-08-05 14:17:05 +0530

    Update Solutions.java

--- Arrays/Solutions.java
+++ Arrays/Solutions.java
@@ -7,3 +7,11 @@ class Solution {
         return new Pair<>(minimum,maximum);
     }
 }
+//Third largest element
+class Solution {
+    int thirdLargest(int arr[]) {
+        // Your code here
+        Arrays.sort(arr);
+        return(arr[arr.length-3]);
+    }
+}
